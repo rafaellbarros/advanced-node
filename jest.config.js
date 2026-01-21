f89b180801f912f93961b11600dc7d9f82ba10b0
Rafael Barros <rafaelbarros.df@gmail.com>
2026-01-21 18:43:36 -0300

    🧪 test: setup jest and add jest, ts-jest and @types/jest dependencies

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+
+/** @type {import('jest').Config} */
+const config = {
+
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  moduleNameMapper: {
+    '@/(.+)': '<rootDir>/src/$1',
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+  },
+  roots: [
+    "<rootDir>/tests",
+    "<rootDir>/tests",
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+};
+
+module.exports = config;
