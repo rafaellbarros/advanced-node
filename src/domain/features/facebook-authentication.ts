094de8491da1bef9bb469e52a8d7cb2af624a2a3
Rafael Barros <rafaelbarros.df@gmail.com>
2026-02-02 20:30:26 -0300

    🧪 test: add facebook-authentication tests

--- src/domain/features/facebook-authentication.ts
+++ src/domain/features/facebook-authentication.ts
@@ -2,12 +2,10 @@ import type { AuthenticationError } from '@/domain/errors'
 import type { AccessToken } from '@/domain/models'
 
 export interface FacebookAuthentication {
-  perform: (
-    params: FacebookAuthentication.Params
-  ) => Promise<FacebookAuthentication.Result>
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
 }
 
-namespace FacebookAuthentication {
+export namespace FacebookAuthentication {
   export type Params = {
     token: string
   }
